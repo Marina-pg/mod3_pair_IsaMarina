588aca950eb9ff4ebab273a11bf554f56f9f0217
Frailin Baez <marina93pg@gmail.com>
2025-04-03 11:11:38 +0200

    pair jueves

--- ETL/main.py
+++ ETL/main.py
@@ -5,3 +5,14 @@ df_clientes = sp.pd.read_csv("../files/clientes.csv", index_col=0)
 df_ventas = sp.pd.read_csv("../files/ventas.csv", index_col=0)
 df_productos = sp.pd.read_csv("../files/productos.csv", delimiter=",", index_col=0)
 print("Descarga de csv´s")
+
+
+lista_dfs = [df_clientes, df_ventas, df_productos]
+
+for l in lista_dfs:
+    sp.buscar_duplicados(l)
+    print(f'Buscando duplicados en {l}')
+
+
+sp.cambiar_a_unk(df_clientes,df_clientes.columns)
+print('cambio nulos por unknown en df_clientes')
--- ETL/src/soporte.py
+++ ETL/src/soporte.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+def buscar_duplicados(df):
+    print(f'Este DataFrame tiene {df.duplicated().sum()} filas duplicadas')
+
 def cambiar_a_unk(df,columnas):
 
     for c in columnas:
